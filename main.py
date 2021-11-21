55024ef39d352cd906f59dba400c7b43ccffcd0a
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-20 17:44:38 -0800

    ask for input/create  list/print list

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ with open("nato_phonetic_alphabet.csv") as file:
 alphabet_main_list.pop(0)
 
 alphabet_dict = {item[0]: item[2::] for item in alphabet_main_list}
+
+user_word = input("Enter a word: ").upper()
+
+new_list = [alphabet_dict[letter] for letter in user_word]
+
+print(new_list)
