f752d1228e9a9c626c5a736e033d371d7c61fe80
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-20 17:43:05 -0800

    create dictionary

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 with open("nato_phonetic_alphabet.csv") as file:
     alphabet_main_list = file.read().split("\n")
 alphabet_main_list.pop(0)
+
+alphabet_dict = {item[0]: item[2::] for item in alphabet_main_list}
