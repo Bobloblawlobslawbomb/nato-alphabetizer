1de93e62a97dec2e23b4f9ad1d50f90074781d42
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-26 16:13:49 -0800

    add error handling to main

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ alphabet_main_list.pop(0)
 
 alphabet_dict = {item[0]: item[2::] for item in alphabet_main_list}
 
-user_word = input("Enter a word: ").upper()
 
-new_list = [alphabet_dict[letter] for letter in user_word]
+def generate_phonetic():
+    user_word = input("Enter a word: ").upper()
 
-print(new_list)
+    try:
+        new_list = [alphabet_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(new_list)
+
+
+generate_phonetic()
